0a29069d7a3c92ecd01cd08e498efe6326e61524
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-04-26 14:54:54 +0200

    Project outline

---
+++ src/component_tree/HomePage.tsx
@@ -0,0 +1,14 @@
+const HomePage = () => (
+  <div>
+    <h1>Landing</h1>
+    <h1>Vision</h1>
+    <h1>Introduction</h1>
+    <h1>Values</h1>
+    <h1>Sponsors</h1>
+    <h1>Blog</h1>
+    <h1>Donations</h1>
+    <h1>Footer</h1>
+  </div>
+)
+
+export default HomePage;
