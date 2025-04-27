4ec3447aa4db08b4d2ecede311bdad965acc57aa
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-04-27 15:40:27 +0200

    Footer basic style

--- src/component_tree/Footer/Footer.tsx
+++ src/component_tree/Footer/Footer.tsx
@@ -1,12 +1,15 @@
+import "./Footer.css";
+
 const Footer = () => (
-  <div>
-    <div>
-      <h1>Stories Inbetween</h1>
+  <div id="Footer">
+    <h1>Stories Inbetween</h1>
+
+    <div className="footer_text">
       <p>The two of us have been traveling South Africa together for the past four years and are now embarking on a walking journey accross the entire country. Follow our social media to see where our adventures take us. Read our blog to get the full story. Support our funding page while we're setting up our shop.</p>
       <p>♥ Have a beautiful day! ♥</p>
     </div>
     
-    <div>
+    <div className="socials">
       <p>TikTok</p>
       <p>YouTube</p>
       <p>Facebook</p>
