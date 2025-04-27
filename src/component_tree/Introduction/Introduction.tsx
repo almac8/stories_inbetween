f6dec23868242c7a2f6ab8e0a8d46e301cbd0edf
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-04-27 14:25:06 +0200

    Introduction basic style

--- src/component_tree/Introduction/Introduction.tsx
+++ src/component_tree/Introduction/Introduction.tsx
@@ -1,9 +1,15 @@
+import "./Introduction.css";
+
 const Introduction = () => (
-  <div>
-    <h1>Who we are</h1>
-    <p>We are Aimée and Sasha, together we have been backpacking South Africa for the past four years. Together we have hiked along most of the South African coast, and now, we would like to invite you along as we share our journey with the world.</p>
-    <p>Our route will take us through a large portion of the country we have not seen yet, before returning to the coast where we will reconnect with old friends.</p>
-    <button>Support our fundraising</button>
+  <div id="Introduction">
+    <div className="image" />
+
+    <div className="box">
+      <h1>Who we are</h1>
+      <p>We are Aimée and Sasha, together we have been backpacking South Africa for the past four years. Together we have hiked along most of the South African coast, and now, we would like to invite you along as we share our journey with the world.</p>
+      <p>Our route will take us through a large portion of the country we have not seen yet, before returning to the coast where we will reconnect with old friends.</p>
+      <button>Support our fundraising</button>
+    </div>
   </div>
 );
 
