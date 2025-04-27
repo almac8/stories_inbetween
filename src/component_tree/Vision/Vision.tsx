f203bb2d48e760a2dbc872617f80baffeefa6bff
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-04-27 13:44:49 +0200

    Vision basic style

--- src/component_tree/Vision/Vision.tsx
+++ src/component_tree/Vision/Vision.tsx
@@ -1,5 +1,7 @@
+import "./Vision.css";
+
 const Vision = () => (
-  <div>
+  <div id="Vision">
     <h1>Join our vision</h1>
     <p>Gender Based Violence has many faces, and with this leg of our journey we aim to unmask as many of them as possible. Join us on our journey of more than 2500km as we walk accross South Africa raising funds and awareness about a very dire situation. Follow our social media, share our page, or donate to help fight for a safer South Africa for all of us.</p>
     <button>Support our cause</button>
