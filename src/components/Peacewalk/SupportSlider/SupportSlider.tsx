dd6ab30ec33ae9d44334f4088d719be8f56ed113
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-09-01 13:08:08 +0200

    Basic peacewalk slider functionality

--- src/components/Peacewalk/SupportSlider/SupportSlider.tsx
+++ src/components/Peacewalk/SupportSlider/SupportSlider.tsx
@@ -1,9 +1,44 @@
+import { useState } from 'react'
 import './SupportSlider.css'
 
-const SupportSlider = () => (
-  <div id="support-slider">
-    <p>Support Slider Component</p>
-  </div>
-)
+const SupportSlider = () => {
+  let [getCurrentlyDisplayedSupporter, setCurrentlyDisplayedSupporter] = useState(0)
+
+  const supporters = [{
+    name: 'Supporter 1',
+    location: 'South Africa',
+    image_source: 'https://images2.imgbox.com/85/f3/SKZmawOp_o.jpg',
+    link: "Website url"
+  }, {
+    name: 'Supporter 2',
+    location: 'Also South Africa',
+    image_source: 'https://images2.imgbox.com/85/f3/SKZmawOp_o.jpg',
+    link: "Website url"
+  }]
+  
+  return (
+    <div id="support-slider">
+      <button onClick={() => setCurrentlyDisplayedSupporter(getCurrentlyDisplayedSupporter > 0 ? getCurrentlyDisplayedSupporter - 1 : supporters.length - 1) }>
+        { '<' }
+      </button>
+
+      {supporters.map((supporter, index) => (
+        <div
+        key={'supporter_' + index}
+        className={ index == getCurrentlyDisplayedSupporter ? 'open' : 'closed' }
+        style={ { backgroundImage: `url(${supporter.image_source})` } }
+        >
+          <p>{ supporter.name }</p>
+          <p>{ supporter.location }</p>
+          <a href={ supporter.link }>Website</a>
+        </div>
+      ))}
+
+      <button onClick={() => setCurrentlyDisplayedSupporter(getCurrentlyDisplayedSupporter < supporters.length - 1 ? getCurrentlyDisplayedSupporter + 1 : 0) }>
+        { '>' }
+      </button>
+    </div>
+  )
+}
 
 export default SupportSlider
