660949e1ad94bd3da779ec3bfd4b3bc24f4c2eb9
Aimée MacDonald <aimeelmacdonald@pm.me>
2025-09-08 14:02:50 +0200

    Social links

--- src/components/LandingPage/SocialBar/SocialBar.tsx
+++ src/components/LandingPage/SocialBar/SocialBar.tsx
@@ -2,10 +2,10 @@ import "./SocialBar.css"
 
 const SocialBar = () => (
   <ul id="social-bar">
-    <li> Facebook </li>
-    <li> Instagram </li>
-    <li> TikTok </li>
-    <li> Youtube </li>
+    <li><a target='_blank' href='https://www.facebook.com/share/16Ysb9npYF/'>Facebook</a></li>
+    <li><a target='_blank' href='https://www.instagram.com/stories.inbetween/'>Instagram</a></li>
+    <li><a target='_blank' href='https://www.tiktok.com/@stories.inbetween'>TikTok</a></li>
+    <li><a target='_blank' href='https://youtube.com/@storiesinbetween'>Youtube</a></li>
   </ul>
 )
 
--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,15 +1,23 @@
 import './Socials.css'
-import StarText from '../StarText/StarText'
 
 const Socials = () => (
   <div id='socials'>
-    <StarText text='Our Socials' />
+    <ul id='socials_list'>
+      <li className='social_block'>
+        <a target='_blank' href='https://www.facebook.com/share/16Ysb9npYF/'>Facebook</a>
+      </li>
 
-    <ul>
-      <li> Facebook </li>
-      <li> Instagram </li>
-      <li> TikTok </li>
-      <li> Youtube </li>
+      <li className='social_block'>
+        <a target='_blank' href='https://www.instagram.com/stories.inbetween/'>Instagram</a>
+      </li>
+
+      <li className='social_block'>
+        <a target='_blank' href='https://www.tiktok.com/@stories.inbetween'>TikTok</a>
+      </li>
+
+      <li className='social_block'>
+        <a target='_blank' href='https://youtube.com/@storiesinbetween'>Youtube</a>
+      </li>
     </ul>
   </div>
 )
