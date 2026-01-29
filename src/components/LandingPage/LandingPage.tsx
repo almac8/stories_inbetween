cd791cf0df5677ba04c3e9ce13cb34a153ad0052
Aimée MacDonald <aimeelmacdonald@pm.me>
2026-01-29 22:02:16 +0200

    Hero background image

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,16 +1,12 @@
 import './LandingPage.css'
-import SpeechBubbleLeft from './SpeechBubbleLeft/SpeechBubbleLeft'
-import SpeechBubbleRight from './SpeechBubbleRight/SpeechBubbleRight'
+
 import SocialBar from './SocialBar/SocialBar'
+
 const LandingPage = () => (
   <div id='landing-page'>
     <SocialBar />
     <h1>Stories in Between</h1>
     <h2>Do something</h2>
-    
-    <SpeechBubbleLeft text='Everybody wants to change the world..' />
-    <p>Toilet roll image</p>
-    <SpeechBubbleRight text='..nobody wants to change the toilet roll.' />
   </div>
 )
 
--- src/components/LandingPage/SpeechBubbleRight/SpeechBubbleRight.tsx
+++
@@ -1,9 +0,0 @@
-import './SpeechBubbleRight.css'
-
-const SpeechBubbleRight = ({ text }: { text: string }) => (
-  <div id='speech_bubble_right_border'>
-    <p id="speech_bubble_right">{ text }</p>
-  </div>
-)
-
-export default SpeechBubbleRight
