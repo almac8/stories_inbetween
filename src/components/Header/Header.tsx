2ada2cedfd0314bec5a68c0f0a836ce76ca1cb14
Aimée MacDonald <aimeelmacdonald@pm.me>
2026-02-02 20:03:35 +0200

    Navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,23 @@
+import './Header.css'
+
 import hamburger from '../../assets/hamburger.svg'
+import { useState } from 'react'
 
-const Header = () => (
-  <header>
-    <img src={ hamburger } alt="Menu" />
+const Header = () => {
+  const [ nav_is_open, set_nav_is_open ] = useState(false)
 
-    <nav>
-      <a href="#who-we-are">Who We Are</a>
-      <a href="#what-we-do">What We Do</a>
-      <a href="#follow-us">Follow Us</a>
-      <a href="#contact-us">Contact Us</a>
-    </nav>
-  </header>
-)
+  return (
+    <header id='header'>
+      <img src={ hamburger } alt="Menu" onClick={() => set_nav_is_open(!nav_is_open)} />
+
+      <nav className={ nav_is_open ? 'open' : 'closed' }>
+        <a href="#who_we_are_section">Who We Are</a>
+        <a href="#what_we_do_section">What We Do</a>
+        <a href="#follow_us_section">Follow Us</a>
+        <a href="#contact_us_section">Contact Us</a>
+      </nav>
+    </header>
+  )
+}
 
 export default Header
