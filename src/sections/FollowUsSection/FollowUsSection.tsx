c2768dd52dbe134e0e6bb2714d48f23208735685
Aimée MacDonald <aimeelmacdonald@pm.me>
2026-02-22 14:05:51 +0200

    Blank target social links

--- src/sections/FollowUsSection/FollowUsSection.tsx
+++ src/sections/FollowUsSection/FollowUsSection.tsx
@@ -9,35 +9,35 @@ import whatsapp_qr from '../../assets/whatsapp_qr.png'
 const FollowUsSection = () => (
   <ul id='follow_us_section'>
     <li>
-      <a href="https://www.facebook.com/share/1BYYGZEymS/">
+      <a href="https://www.facebook.com/share/1BYYGZEymS/" target="_blank" rel="noopener noreferrer">
         <img src={ facebook_logo } alt="Facebook logo" />
         <p>Facebook</p>
       </a>
     </li>
 
     <li>
-      <a href="https://www.instagram.com/stories.inbetween/">
+      <a href="https://www.instagram.com/stories.inbetween/" target="_blank" rel="noopener noreferrer">
         <img src={ instagram_logo } alt="Instagram logo" />
         <p>Instagram</p>
       </a>
     </li>
 
     <li>
-      <a href="https://www.tiktok.com/@stories.inbetween/">
+      <a href="https://www.tiktok.com/@stories.inbetween/" target="_blank" rel="noopener noreferrer">
         <img src={ tiktok_logo } alt="TikTok logo" />
         <p>TikTok</p>
       </a>
     </li>
 {/* 
     <li>
-      <a href="#">
+      <a href="#" target="_blank" rel="noopener noreferrer">
         <img src={ youtube_logo } alt="YouTube logo" />
         <p>TikTok</p>
       </a>
     </li>
  */}
     <li>
-      <a href="https://chat.whatsapp.com/BbJM5ekaXdJJ1uyf8HhlE4/">
+      <a href="https://chat.whatsapp.com/BbJM5ekaXdJJ1uyf8HhlE4/" target="_blank" rel="noopener noreferrer">
         <img src={ whatsapp_qr } alt="WhatsApp QR code" />
         <p>WhatsApp</p>
       </a>
